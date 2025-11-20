0d4c866b5e02f2286e748e58814c5cd2937f0983
Orrest Xu <orrest.xu@bbkco.com.cn>
2025-11-20 17:22:04 +0800

    Medias: Async play.

--- Tests.Wpf/Medias/PlayerViewModel.cs
+++ Tests.Wpf/Medias/PlayerViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LibVLCSharp.Shared;
+using Tests.Wpf.Threadings;
 
 namespace Tests.Wpf.Medias;
 
@@ -51,6 +53,33 @@ public sealed partial class PlayerViewModel : ObservableRecipient, IDisposable
         Messenger.Send($"Index {currentIndex}, playing...", nameof(MainViewModel));
 
         currentPlayer?.Play(media);
+    }
+
+    [RelayCommand]
+    private async Task Play1()
+    {
+        if (currentIndex >= sounds.Count)
+        {
+            Messenger.Send($"Index {currentIndex}, list over.", nameof(MainViewModel));
+            return;
+        }
+
+        var sound = sounds[currentIndex];
+        using var media = new Media(vlc, sound.FullName);
+
+        Messenger.Send($"Index {currentIndex}, playing...", nameof(MainViewModel));
+        Messenger.Send($"On thread {Environment.CurrentManagedThreadId}, playing...", nameof(MainViewModel));
+
+        currentPlayer = new MyMediaPlayer(vlc);
+        bool finished = await ((MyMediaPlayer)currentPlayer).PlayAsync(media);
+
+        Messenger.Send($"On thread {Environment.CurrentManagedThreadId}, finished...", nameof(MainViewModel));
+
+        if (finished)
+        {
+            currentIndex++;
+            await Play1();
+        }
     }
 
     [RelayCommand]
---
+++ Tests.Wpf/Threadings/MyMediaPlayer.cs
@@ -0,0 +1,44 @@
+﻿using LibVLCSharp.Shared;
+
+namespace Tests.Wpf.Threadings;
+
+public class MyMediaPlayer : MediaPlayer
+{
+    protected TaskCompletionSource<bool> endReachedTask = null!;
+
+    public MyMediaPlayer(LibVLC libVLC)
+        : base(libVLC) { }
+
+    public async Task<bool> PlayAsync(Media media)
+    {
+        endReachedTask = new TaskCompletionSource<bool>();
+
+        EndReached += OnEndReached;
+        EncounteredError += OnError;
+
+        var playResult = Play(media);
+        if (!playResult)
+        {
+            EndReached -= OnEndReached;
+            EncounteredError -= OnError;
+
+            return false;
+        }
+
+        return await endReachedTask.Task;
+    }
+
+    private void OnEndReached(object? sender, EventArgs e)
+    {
+        EndReached -= OnEndReached;
+        EncounteredError -= OnError;
+        endReachedTask.TrySetResult(true);
+    }
+
+    private void OnError(object? sender, EventArgs e)
+    {
+        EndReached -= OnEndReached;
+        EncounteredError -= OnError;
+        endReachedTask.TrySetResult(false);
+    }
+}
