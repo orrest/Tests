5a0184cdc16d136e3144c102f6ed81b70321f20d
Orrest Xu <orrest.xu@bbkco.com.cn>
2025-11-28 10:02:16 +0800

    Progress mask view.

---
+++ Tests.Wpf/ProgressMask/ProgressMaskView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Tests.Wpf.ProgressMask;
+
+public partial class ProgressMaskView : UserControl
+{
+    public ProgressMaskView()
+    {
+        InitializeComponent();
+        DataContext = new ProgressMaskViewModel();
+    }
+}
---
+++ Tests.Wpf/ProgressMask/ProgressMaskViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Tests.Wpf.Messages;
+
+namespace Tests.Wpf.ProgressMask;
+
+public partial class ProgressMaskViewModel : ObservableRecipient
+{
+    [ObservableProperty]
+    public partial bool IsProgressMaskShown { get; set; }
+
+    [RelayCommand]
+    private void ShowProgressMask()
+    {
+        Messenger.Send(
+            new ProgressMaskMessage(visible: true, caption: "Loading..."),
+            nameof(ProgressMask)
+        );
+    }
+
+    [RelayCommand]
+    private async Task ShowMaskThenManuallyCancelIteration()
+    {
+        var rand = new Random();
+
+        using var source = new CancellationTokenSource();
+        var token = source.Token;
+
+        // begin loading
+        Messenger.Send("Start loading...", nameof(MainViewModel));
+
+        var i = 1;
+        for (; i <= 10; i++)
+        {
+            // if cancelled
+            if (token.IsCancellationRequested)
+            {
+                break;
+            }
+
+            // during loading
+            Messenger.Send(
+                new ProgressMaskMessage(
+                    visible: true,
+                    caption: $"During iteration [{i}]...",
+                    cancellationTokenSource: source
+                ),
+                nameof(ProgressMask)
+            );
+
+            await Task.Delay(rand.Next(500, 1200));
+        }
+
+        // finished
+        var finishMessage = token.IsCancellationRequested
+            ? $"Finished by manually cancellation before [{i}]!"
+            : $"{i - 1} iterations finished!";
+
+        Messenger.Send(finishMessage, nameof(MainViewModel));
+
+        Messenger.Send(new ProgressMaskMessage(visible: false), nameof(ProgressMask));
+    }
+}
