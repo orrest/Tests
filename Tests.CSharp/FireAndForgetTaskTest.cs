1b0f0b75f1845c7dd6912e6752c29d85a9f5d5b2
Orrest Xu <orrest.xu@bbkco.com.cn>
2026-01-16 13:51:05 +0800

    Call awaitable method without await.

--- Tests.CSharp/FireAndForgetTaskTest.cs
+++ Tests.CSharp/FireAndForgetTaskTest.cs
@@ -68,6 +68,20 @@ public class FireAndForgetTaskTest
     }
 
     [TestMethod]
+    public async Task CallAsyncMethodWithoutAwaitMakesItFireAndForget()
+    {
+        Stopwatch stopwatch = new();
+
+        stopwatch.Restart();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        AwaitableMethod();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        stopwatch.Stop();
+
+        Assert.IsLessThan(100, stopwatch.ElapsedMilliseconds);
+    }
+
+    [TestMethod]
     public async Task AwaitableMethodIsAwaitableEventItsInTaskRun()
     {
         static async Task action() { await Task.Delay(DELAY_MILLISECONDS);  }
