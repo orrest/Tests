e1bb5bac18b075536d8c8b1f8be8294e03dde12c
Orrest Xu <orrest.xu@bbkco.com.cn>
2026-01-21 15:03:27 +0800

    Tree view

---
+++ Tests.Wpf/Converters/IsTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Tests.Wpf.Converters;
+
+
+public sealed class IsTypeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is null || parameter is null)
+            return false;
+
+        var targetTypeParam = parameter as Type;
+
+        return targetTypeParam?.IsInstanceOfType(value) == true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
